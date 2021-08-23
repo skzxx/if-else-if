397d0757d51f5453282ad4fd1d08f403953791b1
skzxx <rrachmawan1965@gmail.com>
2021-08-24 01:51:28 +0700

    Add

---
+++ Main.java
@@ -0,0 +1,24 @@
+/*Program if else java*/
+
+public class Main {
+  public static void main (String[] args) {
+    int OperatorAnalisaStatement = 5;
+    /*
+    if (Ekspresi 1) {
+      Statement 1
+    } else if (ekspresi 2) {
+      Statement 2
+    } else {
+      Statement default
+    }
+    
+    */
+    if (OperatorAnalisaStatement == 5) {
+      System.out.print("Aksi jika kasus variabel = 5");
+    } else if (OperatorAnalisaStatement == 10 ) {
+      System.out.print("Aksi jika kasus variabel = 10");
+    } else {
+      System.out.print("Aksi jika kasus variabel tidak sama dengan (≠) ekspresi yang diatas");
+    }
+  }
+}
